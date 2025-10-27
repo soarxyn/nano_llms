ecd9829a894be44afc5fdb1f616537d6b18bf409
Soarxyn <macias.gabriel@proton.me>
2025-10-27 19:24:54 -0300

    feat: incremental file ingestion

--- nano_llms/tokenizer.py
+++ nano_llms/tokenizer.py
@@ -1,14 +1,64 @@
+import os
 from collections import defaultdict
 from dataclasses import dataclass
-from functools import partial
-from typing import Iterator, Self
 from multiprocessing import Pool
+from typing import BinaryIO, Iterator, Self
 
 import regex as re
 from tqdm import trange
 
 # From: https://github.com/karpathy/minbpe/blob/1acefe89412b20245db5a22d2a02001e547dc602/minbpe/gpt4.py#L48C22-L48C138
 GPT4_PAT = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
+
+
+# From: https://github.com/stanford-cs336/assignment1-basics/blob/main/cs336_basics/pretokenization_example.py
+def find_chunk_boundaries(
+    file: BinaryIO,
+    desired_num_chunks: int,
+    split_special_token: bytes,
+) -> list[int]:
+    """
+    Chunk the file into parts that can be counted independently.
+    May return fewer chunks if the boundaries end up overlapping.
+    """
+    assert isinstance(split_special_token, bytes), (
+        "Must represent special token as a bytestring"
+    )
+
+    # Get total file size in bytes
+    file.seek(0, os.SEEK_END)
+    file_size = file.tell()
+    file.seek(0)
+
+    chunk_size = file_size // desired_num_chunks
+
+    # Initial guesses for chunk boundary locations, uniformly spaced
+    # Chunks start on previous index, don't include last index
+    chunk_boundaries = [i * chunk_size for i in range(desired_num_chunks + 1)]
+    chunk_boundaries[-1] = file_size
+
+    mini_chunk_size = 4096  # Read ahead by 4k bytes at a time
+
+    for bi in range(1, len(chunk_boundaries) - 1):
+        initial_position = chunk_boundaries[bi]
+        file.seek(initial_position)  # Start at boundary guess
+        while True:
+            mini_chunk = file.read(mini_chunk_size)  # Read a mini chunk
+
+            # If EOF, this boundary should be at the end of the file
+            if mini_chunk == b"":
+                chunk_boundaries[bi] = file_size
+                break
+
+            # Find the special token in the mini chunk
+            found_at = mini_chunk.find(split_special_token)
+            if found_at != -1:
+                chunk_boundaries[bi] = initial_position + found_at
+                break
+            initial_position += mini_chunk_size
+
+    # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
+    return sorted(set(chunk_boundaries))
 
 
 @dataclass
@@ -73,7 +123,9 @@ class MergeJob:
 
 
 class Tokenizer:
-    def __init__(self, vocabulary_size: int, *, pat: str | None = None):
+    def __init__(
+        self, vocabulary_size: int, *, pat: str | None = None, num_workers: int = 12
+    ):
         assert vocabulary_size > 256, (
             "Vocabulary size must be over the default 256 Unicode Bytes."
         )
@@ -81,11 +133,15 @@ class Tokenizer:
         self.vocabulary_size = vocabulary_size
         self.pat = re.compile(pat or GPT4_PAT)
 
+        self.eot_pat = re.compile(re.escape("<|endoftext|>"))
+
         self.merges: dict[tuple[int, int], int] = {}
         self.vocabulary = {token: bytes([token]) for token in range(256)}
 
         self.special_tokens: dict[str, int] = {}
         self.inverse_special_tokens: dict[int, str] = {}
+
+        self.num_workers = num_workers
 
     # def get_pair_counts(self, tokens: list[int]) -> dict[tuple[int, int], int]:
     #     pair_counts: dict[tuple[int, int], int] = defaultdict(int)
@@ -109,7 +165,7 @@ class Tokenizer:
 
             return local_counts, local_index
 
-        with Pool(processes=12) as pool:
+        with Pool(processes=self.num_workers) as pool:
             local_results = pool.starmap(map_count, (words, counts))
 
         pair_counts: dict[tuple[int, int], int] = defaultdict(int)
@@ -159,12 +215,41 @@ class Tokenizer:
 
         return new_tokens, deltas
 
-    def train(self, train_dataset: str, *, verbose: bool = False):
-        text_chunks: list[str] = self.pat.findall(train_dataset)
+    def train(self, dataset_path: str, *, verbose: bool = False):
+        counts: dict[str, int] = defaultdict(int)
 
-        token_chunks: list[list[int]] = [
-            list(chunk.encode("utf-8", errors="ignore")) for chunk in text_chunks
-        ]
+        def count_in_chunk(strings: str):
+            local_counts: dict[str, int] = defaultdict(int)
+
+            for atom in self.pat.findall(strings):
+                local_counts[atom] += 1
+
+            return local_counts
+
+        with open(dataset_path, mode="rb") as fp:
+            boundaries = find_chunk_boundaries(
+                fp, self.num_workers * 256, b"<|endoftext|>"
+            )
+
+            for start, end in zip(boundaries[:-1], boundaries[1:]):
+                fp.seek(start)
+                chunk = fp.read(end - start).decode("utf-8", errors="ignore")
+
+                strings = self.eot_pat.split(chunk)
+
+                with Pool(processes=self.num_workers) as pool:
+                    local_counts = pool.map(count_in_chunk, strings)
+
+                for local_count in local_counts:
+                    for word, count in local_count.items():
+                        counts[word] += count
+
+        words: list[Word] = []
+        count_vector: list[int] = []
+
+        for word, count in counts.items():
+            words.append(Word(list(word.encode("utf-8", errors="ignore"))))
+            count_vector.append(count)
 
         num_merges = self.vocabulary_size - 256
 
