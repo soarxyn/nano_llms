6ee51f59c37016372d22c5f2402802977e961cd1
Soarxyn <macias.gabriel@proton.me>
2025-11-02 15:34:52 -0300

    feat: stable softmax

---
+++ nano_llms/softmax.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class Softmax(nn.Module):
+    def forward(self, x: torch.Tensor, dim: int) -> torch.Tensor:
+        max_val, _ = x.max(dim=dim, keepdim=True)
+        z = (x - max_val).exp()
+
+        return z / z.sum(dim=dim, keepdim=True)
